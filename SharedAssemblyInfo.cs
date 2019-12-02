4bcfe85de128843c3f05e4acb6f0f81ee584ea66
Valeriano A.R <valen.alfonso@gmail.com>
2019-12-02 16:03:39 +0100

    0.2.0

---
+++ SharedAssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("VAR")]
+[assembly: AssemblyCopyright("Copyright © VAR 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
